0113e93d9b5df09871ded16821d9e6a5d96ee670
PikaLegend_ <76042212+PikaTheLeg@users.noreply.github.com>
2022-03-03 16:14:39 +1100

    aaaaaaaaaa
    a

--- search.js
+++ search.js
@@ -1,20 +1,18 @@
-function readTextFile(file)
-{
-    var rawFile = new XMLHttpRequest();
-    rawFile.open("GET", file, false);
-    rawFile.onreadystatechange = function ()
-    {
-        if(rawFile.readyState === 4)
-        {
-            if(rawFile.status === 200 || rawFile.status == 0)
-            {
-                var allText = rawFile.responseText;
-                return allText;
-            }
-        }
-    }
-    rawFile.send(null);
-}
+var Dropbox = require('dropbox');
+var dbx = new Dropbox({accessToken: 'sl.BDFmXn2QZ8RDvrTvRKoOXyMyeEV7XSkCJH8W6YPGh3HAdP-GI-eVAvssgBLWwDuTG4TCjqdKMBMK6YeUC315ms2svoHPNp46hVqy6vyu0Is1p8gV-VL2GNpkVJ1llEDKxQCAzOE'});
+
+dbx.filesDownload({path: '/test.txt'})
+    .then(function (response) {
+        var blob = response.fileBlob;
+        var reader = new FileReader();
+        reader.addEventListener("loadend", function() {
+            console.log(reader.result); // will print out file content
+        });
+        reader.readAsText(blob);
+    })
+    .catch(function (error) {
+        //something
+    })
 
 let data = readTextFile("https://www.dropbox.com/s/nzrwxhj7qkrsimb/data.csv?dl=0");
 console.log(data);
